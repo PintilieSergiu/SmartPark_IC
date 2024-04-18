a1abffef841b803d27454c4b292a29df322f6e10
Andreea <nicoleta.petrucian@student.upt.ro>
2024-04-18 11:59:53 +0300

    Register,Login and BarcodeGeneration Pages

--- Frontend/SmartParkApp/app/build.gradle.kts
+++ Frontend/SmartParkApp/app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
 }
 
 dependencies {
-
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation ("com.google.zxing:core:3.3.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
